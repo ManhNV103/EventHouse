53c98b524057281d5d597177c78068a3d57cf606
Manh Nguyen <manhnguyen@Manhs-MacBook-Pro.local>
2020-09-06 00:32:57 +1000

    finish setting up semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { cars } from './demo';
-import CarItem from './CarItem';
-import { render } from '@testing-library/react';
-import axios from 'axios';
-import { REPL_MODE_SLOPPY } from 'repl';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { render } from "@testing-library/react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
 class App extends React.Component {
   state = {
-    values: []
-  }
+    values: [],
+  };
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-    .then((response) => {
+    axios.get("http://localhost:5000/api/values").then((response) => {
       this.setState({
-        values: response.data
-      })
-    })
+        values: response.data,
+      });
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <ul>
-          {cars.map((car) => {
-            return <CarItem car={car}/>;
-          })}
-        </ul>
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) =>
+            value ? <List.Item key={value.id}>{value.name}</List.Item> : null
+          )}
+        </List>
       </div>
     );
   }
