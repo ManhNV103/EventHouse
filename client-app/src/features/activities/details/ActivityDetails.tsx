5d007c6f89c0177a30f6352a88f3140b4b9f3566
Manh Nguyen <manhnguyen@Manhs-MacBook-Pro.local>
2020-09-30 21:48:25 +1000

    add route

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,11 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Card, Image, Button } from "semantic-ui-react";
 import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
+import { RouteComponentProps } from "react-router-dom";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-const ActivityDetails: React.FC = () => {
+interface DetailsParams {
+  id: string
+}
+
+const ActivityDetails: React.FC<RouteComponentProps<DetailsParams>> = ({match}) => {
   const activityStore = useContext(ActivityStore);
-  const { selectedActivity: activity, openEditForm, cancelSelectedActivity } = activityStore;
+  const { activity, openEditForm, cancelSelectedActivity, loadActivity, loadingInitial } = activityStore;
+
+  useEffect(() => {
+    loadActivity(match.params.id)
+  }, [loadActivity])
+
+  if (loadingInitial || !activity) return <LoadingComponent>Loading activity...</LoadingComponent>
+
   return (
     <Card fluid>
       <Image
